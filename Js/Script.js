26abd45dec87019a10d6136b5258ee2181e45ae6
HenriqueCesarS <rm94276@fiap.com.br>
2022-07-25 20:09:04 -0300

    atualização script e css

--- Js/Script.js
+++ Js/Script.js
@@ -12,5 +12,27 @@ const btnLimpar = document.getElementById('limparId')
 
 btnLimpar.addEventListener('click', ()=>{
     document.getElementById('converterValorId').value = ""; 
-    document.getElementById('resultadoId').innerHTML= "(dec)";
+    document.getElementById('resultadoId').innerHTML= "(bin)";
+})
+
+function binDec(num){
+    var num = parseInt(document.getElementById('converterValorId').value) 
+
+}
+
+//Função para a conta em Binário para decimal
+const btnBinDec = document.getElementById('binDec')
+
+btnBinDec.addEventListener('click', ()=>{
+    document.getElementById('nmAlteracaone').innerHTML = " Binário";
+    document.getElementById('nmAlteracaot').innerHTML = " Decimal";
+    document.getElementById('converterValorId').placeholder = "(bin)";
+    document.getElementById('resultadoId').innerHTML = "(dec)";
+    let btnConfirm = document.getElementById('confirmId')
+    
+    btnConfirm.addEventListener('click', ()=>{
+        var valor = parseInt(document.getElementById('converterValorId').value)
+        var resultadoBinDec = binDec(valor)
+        document.getElementById('resultadoId').innerHTML= resultadoBinDec;
+    })
 })
